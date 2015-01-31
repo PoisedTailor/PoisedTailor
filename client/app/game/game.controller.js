ef125626d1216fbce52c25f7f5e27969b9278a15
Philip Rosen <philipjrosen@gmail.com>
2015-01-30 19:32:12 -0800

    implement cell swapping in view

--- client/app/game/game.controller.js
+++ client/app/game/game.controller.js
@@ -2,6 +2,30 @@
 
 angular.module('projectApp')
   .controller('GameCtrl', function ($scope) {
+    $scope.cellsToSwap = [];
+
     var board = new Board();
     $scope.grid = board.get('state');
+
+    $scope.getCell = function(row, col) {
+      $scope.cellsToSwap.push([row, col]);
+      if ($scope.cellsToSwap.length === 2) {
+        $scope.swap();
+      }
+    };
+
+    $scope.swap = function() {
+      var isValid = board.isValidSwap(
+        $scope.cellsToSwap[0],
+        $scope.cellsToSwap[1]
+      );
+      if(isValid) {
+        board.swap($scope.cellsToSwap[0],
+        $scope.cellsToSwap[1]);
+        $scope.cellsToSwap = [];
+      } else {
+        alert("Can't swap that pair! Try again.");
+        $scope.cellsToSwap = [];
+      }
+    };
   });
